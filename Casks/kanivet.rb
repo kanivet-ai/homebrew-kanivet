9f59e01622aaec84b012a4b2e18f758ee22f0072
Giovanni Ferri <giovanni@syscode.uk>
2026-09-08 19:53:27 +0100

    Fix deprecated depends_on macos string comparison

--- Casks/kanivet.rb
+++ Casks/kanivet.rb
@@ -16,7 +16,7 @@ cask "kanivet" do
   end
 
   auto_updates true
-  depends_on macos: ">= :monterey"
+  depends_on macos: :monterey
 
   app "kanivet-standalone.app"
 
--- official-cask-submission/kanivet.rb
+++ official-cask-submission/kanivet.rb
@@ -20,7 +20,7 @@ cask "kanivet" do
   end
 
   auto_updates true
-  depends_on macos: ">= :monterey"
+  depends_on macos: :monterey
 
   app "kanivet.app"
 
